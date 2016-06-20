6dbcd169b906a408ff460ca0be61e756064d7b52
Jen Solima <jensolima@gmail.com>
2016-06-20 11:43:52 -0500

    employee create complete

--- app/Controllers/EmployeeController.js
+++ app/Controllers/EmployeeController.js
@@ -3,15 +3,76 @@
 LiteSupport.controller('EmployeeController', [
   '$http', 
   '$scope',
+  '$location',
 
-  function ($http, $scope) {
+  function ($http, $scope, $location) {
 
     $scope.employees = [];
 
+    //Get employee list and department
     $http
       .get('http://localhost:5000/api/Employee')
       .success(emp => $scope.employees = emp);
 
-    
+    $http
+      .get('http://localhost:5000/api/Department')
+      .success(dep => $scope.departments = dep);
+
+    // add new employee
+    $scope.addEmployee = function () {
+      $http({
+        url:'http://localhost:5000/api/Employee',
+        method: 'POST',
+        data: JSON.stringify({
+            FirstNameE: $scope.emp.FirstNameE,
+            LastNameE: $scope.emp.LastNameE,
+            Username: $scope.emp.Username,
+            EmailE: $scope.emp.EmailE,
+            PhoneE: $scope.emp.PhoneE,
+            DepartmentId: $scope.emp.DepartmentId
+          })
+      })
+      .success(emp => console.log('201 Created', emp))
+      .then(function() {
+         $location.path("/employees")
+      })
+    };
+
+    // view employee details
+    $scope.detailEmployee = function (id) {
+      console.log(id);
+      $http({
+        method: 'GET',
+        url:`http://localhost:5000/api/Employee/${id}`
+      })
+      .success( function(emp) {
+        $scope.$parent.emp = emp[0];        
+        console.log("$scope.$parent.emp", $scope.$parent.emp[0]);
+        // console.log("emp", emp);
+      })
+      .then(function() {
+        $location.path("/details-employee")
+      })
+    }
+
+    // // edit employee details
+    // $scope.editEmployee = function (id) {
+    //    console.log("id",id);
+    //    $http({
+    //       method: 'GET',
+    //       url:`http://localhost:5000/api/Employee/${id}`
+    //     })
+    //   .success( function(emp) {
+    //        // console.log("emp", emp);
+    //       $scope.$parent.emp = emp;        
+    //   })
+    //   .then(function() {
+    //       $location.path("/edit-employee")
+    //   })
+    // }
+
+
+
+
   }
 ]);
--- app/app.js
+++ app/app.js
@@ -20,12 +20,28 @@ LiteSupport.config(['$routeProvider',
         templateUrl: 'partials/add-employee.html',
         controller: 'EmployeeController'
       })
+      .when('/details-employee', {
+        templateUrl: 'partials/details-employee.html',
+        controller: 'EmployeeController'
+      })
+      .when('/edit-employee', {
+        templateUrl: 'partials/edit-employee.html',
+        controller: 'EmployeeController'
+      })
       .when('/customers', {
         templateUrl: 'partials/customers.html',
         controller: 'CustomerController'
       })
       .when('/add-customer', {
         templateUrl: 'partials/add-customer.html',
+        controller: 'CustomerController'
+      })
+      .when('/details-customer', {
+        templateUrl: 'partials/details-customer.html',
+        controller: 'CustomerController'
+      })
+      .when('/edit-customer', {
+        templateUrl: 'partials/edit-customer.html',
         controller: 'CustomerController'
       })
       .when('/login', {
